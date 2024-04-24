77acc5a682b94a1411da868998374a0264e05279
sshworld <59462537+sshworld@users.noreply.github.com>
2024-04-24 15:19:32 +0900

    init: 프로젝트 생성

---
+++ user/src/test/java/kr/co/thecommerce/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.thecommerce.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
