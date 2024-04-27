d0880727fa562b01fd959bff355690f51174f97f
sshworld <59462537+sshworld@users.noreply.github.com>
2024-04-27 22:50:36 +0900

    chore: user insert sql 작성

---
+++ user/src/main/resources/db/migration/V2__insert_user.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (id, password, nickname, `name`, mobile_number, email, created_at)
+VALUES ('hong33', 'hong1234', '홍박사를 아세요', '홍길동', '010-1234-5678', 'hong@gmail.com', '2023-11-29 21:16:42');
+
+INSERT INTO users (id, password, nickname, `name`, mobile_number, email, created_at)
+VALUES ('lee2222', 'sunsin12', '명량', '이순신', '010-2313-2222', 'lee22@gmail.com', '2024-04-24 17:27:42');
+
+INSERT INTO users (id, password, nickname, `name`, mobile_number, email, created_at)
+VALUES ('jang3323', 'bogo2231', '엄마장보고왔다', '장보고', '010-9931-3323', 'jang13@naver.com', '2024-02-12 09:51:51');
