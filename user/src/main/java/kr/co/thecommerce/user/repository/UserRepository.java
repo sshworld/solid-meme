390460fa3ab8b8e8711742618cadcca459cc22d8
sshworld <59462537+sshworld@users.noreply.github.com>
2024-04-27 15:33:05 +0900

    feat: 유저 repository 구현

---
+++ user/src/main/java/kr/co/thecommerce/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package kr.co.thecommerce.user.repository;
+
+import kr.co.thecommerce.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
